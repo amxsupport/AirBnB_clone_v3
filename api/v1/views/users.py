3027ea76180a24a87528438769a94438ee559fce
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-04 22:10:29 +0100

    Update users.py implement get_users def

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -9,3 +9,14 @@ from models.user import User
 import os
 app = Flask(__name__)
 
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all('User')
+    users_list = []
+    for user in users.values():
+        users_list.append(user.to_dict())
+    return jsonify(users_list), 200
+
+
