c494ca1a34e3bb08da843f38dde07f1297200532
amxsupport <itmasterdesigne@gmail.com>
2024-02-04 16:28:26 +0000

    create ~/AirBnB_clone_v3/api/v1/views

---
+++ api/v1/views/index.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, jsonify
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+app = Flask(__name__)
+
+
