8f3e36a568c2147d6816eec90198db3762ce9b23
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-04 22:02:55 +0100

    Update amenities.py implement get_amenities def

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,3 +8,15 @@ from api.v1.views import app_views
 from models.amenity import Amenity
 import os
 app = Flask(__name__)
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    """Retrieves the list of all Amenity objects"""
+    amenities = storage.all('Amenity')
+    amenities_list = []
+    for amenity in amenities.values():
+        amenities_list.append(amenity.to_dict())
+    return jsonify(amenities_list), 200
+
+
