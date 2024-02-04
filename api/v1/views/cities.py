2a96e4bf14d85136b32f92066c3e96d2b6ae7c26
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-04 18:32:06 +0100

    Update cities.py implement get_cities def

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,3 +11,20 @@ from models.city import City
 import os
 app = Flask(__name__)
 
+
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id=None):
+    """Retrieves the list of all City objects"""
+    states = storage.all('State')
+    state = states.get('State' + '.' + state_id)
+    if state is None:
+        abort(404)
+    city_list = []
+    cities = storage.all('City')
+    for city in cities.values():
+        if city.state_id == state_id:
+            city_list.append(city.to_dict())
+    return jsonify(city_list), 200
+
+
