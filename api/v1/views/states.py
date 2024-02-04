b76d8bb216f48afb5a5a60e45a26e81a30b0bf60
Ottman <133713821+Otmanbboy@users.noreply.github.com>
2024-02-04 17:55:31 +0100

    Update states.py implement get_states def

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,3 +9,12 @@ from models.state import State
 import os
 app = Flask(__name__)
 
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all('State')
+    state_list = []
+    for state in states.values():
+        state_list.append(state.to_dict())
+    return jsonify(state_list), 200
+
